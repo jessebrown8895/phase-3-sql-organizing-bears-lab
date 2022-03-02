d0927bccc4601435e9ef5082bab48345847468a4
Jesse Brown <Brownjesse8895@yahoo.com>
2022-03-01 19:02:49 -0600

    commit tree 28294bb0bc4da266e075442bb30a552b52c98a49

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 26, "M", "Blue", "Mad", 1),
+("Rowdy", 22, "M", "Red", "Sweet", 0),
+("Tabitha", 24, "F", "Pink", "Nice", 1),
+("Sergeant Brown", 38, "M", "No Favorite", "Put together", 1),
+("Melissa", 18, "F", "Green", "Nice", 1),
+("Grinch", 27, "M", "Green", "Evil", 1),
+("Wendy", 25, "M", "Blue", "Sweet", 0),
+(NULL, 25, "M", "Blue", "Sweet", 0);
